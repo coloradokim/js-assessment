bee225debe35281af23c3fe10b6b109f0e3f579b
coloradokim <schlesinger.kim@gmail.com>
2017-02-20 11:56:14 -0700

    added some array solutions

--- app/arrays.js
+++ app/arrays.js
@@ -2,19 +2,39 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum: function(arr) {
-
+    let result = 0
+    for (var i = 0; i < arr.length; i++) {
+      result += arr[i]
+    }
+    return result;
   },
 
   remove: function(arr, item) {
-
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+      if(arr[i] != item){
+        newArr.push(arr[i])
+      }
+    }
+    return newArr
   },
 
   removeWithoutCopy: function(arr, item) {
+    var i;
+    var len;
 
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === item) {
+        arr.splice(i, 1)
+        i--;
+        len--;
+      }
+    }
+    return arr;
   },
 
   append: function(arr, item) {
